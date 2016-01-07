eaecf0befe5c4bdd5790b40bb3351f7035199416
Der15 <daniel.ross@rogers.com>
2016-01-07 12:55:46 -0500

    Update Golf.java

--- Golf.java
+++ Golf.java
@@ -3,6 +3,7 @@ package com.egs.eddiegolfsim;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Game;
 import com.egs.screens.GameScreen;
+import com.egs.golfhelpers.*;
 
 public class Golf extends Game {
     
@@ -10,7 +11,13 @@ public class Golf extends Game {
     @Override
     public void create() {
         Gdx.app.log("Eddie's Golf Simulator", "created");
+        AssetLoader.load();
         setScreen(new GameScreen());
     }
 	
+     @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
+    }
 }
