a9c37077f3229d3fc0c44291fe35c5e051d441ba
Der15 <daniel.ross@rogers.com>
2016-01-07 12:58:08 -0500

    Update GameRenderer.java

--- GameRenderer.java
+++ GameRenderer.java
@@ -6,64 +6,68 @@ package com.egs.gameworld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.egs.gameobjects.Ball;
+import com.egs.golfhelpers.AssetLoader;
 
 public class GameRenderer {
-
+    
+    private Ball ball;
     private GameWorld myWorld;
     private OrthographicCamera cam;
     private ShapeRenderer shapeRenderer;
+    private SpriteBatch batch;
+    
+    private Texture background;
+    private Texture golfBall;
+    private int midPointY;
+    private int gameHeight;
 
-    public GameRenderer(GameWorld world) {
+    public GameRenderer(GameWorld world, int gameHeight, int midPointY) {
         myWorld = world;
+        this.gameHeight = gameHeight;
+        this.midPointY = midPointY;
+
         cam = new OrthographicCamera();
-        cam.setToOrtho(true, 136, 204);
+        cam.setToOrtho(true, 1024, gameHeight);
+
+        batch = new SpriteBatch();
+        batch.setProjectionMatrix(cam.combined);
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(cam.combined);
 
+        // Call helper methods to initialize instance variables
+        initializeGameObjects();
+        initializeAssets();
     }
 
+    public void initializeGameObjects(){
+        ball = myWorld.getBall();
+    }
+    
+    public void initializeAssets(){
+        background = AssetLoader.bg;
+        golfBall = AssetLoader.ball;
+    }
+    
     public void render() {
         Gdx.app.log("GameRenderer", "render");
-
-        /*
-         * 1. We draw a black background. This prevents flickering.
-         */
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        /*
-         * 2. We draw the Filled rectangle
-         */
-        // Tells shapeRenderer to begin drawing filled shapes
-        shapeRenderer.begin(ShapeType.Filled);
-
-        // Chooses RGB Color of 87, 109, 120 at full opacity
-        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-
-        // Draws the rectangle from myWorld (Using ShapeType.Filled)
-        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
-                myWorld.getRect().width, myWorld.getRect().height);
-
-        // Tells the shapeRenderer to finish rendering
-        // We MUST do this every time.
-        shapeRenderer.end();
-
-        /*
-         * 3. We draw the rectangle's outline
-         */
-        // Tells shapeRenderer to draw an outline of the following shapes
-        shapeRenderer.begin(ShapeType.Line);
-
-        // Chooses RGB Color of 255, 109, 120 at full opacity
-        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
-
-        // Draws the rectangle from myWorld (Using ShapeType.Line)
-        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
-                myWorld.getRect().width, myWorld.getRect().height);
-
-        shapeRenderer.end();
+       // Gdx.gl.glClearColor(0, 0, 0, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        
+        
+        batch.begin();
+        batch.disableBlending();
+        batch.draw(background, 0, midPointY - 23, 10, 10);
+        batch.draw(golfBall, ball.getX(), ball.getY());
+        
+        batch.enableBlending();
+        batch.end();
+        
     }
 
 }
