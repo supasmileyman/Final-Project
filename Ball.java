caf4eb02b457ff4a3e8d4c7eacccb15a275d1fb7
supasmileyman <jlxpi123@gmail.com>
2016-01-13 13:32:52 -0500

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
+import com.egs.golfhelpers.*;
 
 /**
  *
