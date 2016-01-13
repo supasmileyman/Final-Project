f3feb1077dd5a9d42041a221cf8d30517ae0a106
Der15 <daniel.ross@rogers.com>
2016-01-13 13:53:31 -0500

    Update GameScreen.java

--- GameScreen.java
+++ GameScreen.java
@@ -17,30 +17,39 @@ public class GameScreen implements Screen {
 
     private GameWorld world;
     private GameRenderer renderer;
-
+    /**
+     * creates the game screen object, which is the screen itself
+     */
     public GameScreen() {
         float screenWidth = Gdx.graphics.getWidth();
         float screenHeight = Gdx.graphics.getHeight();      
         float gameWidth = 1024;
         float gameHeight = screenHeight / (screenWidth / gameWidth);
-        
         int midPointY = (int) (gameHeight / 2);
-
+        //Intialize a new gameworld and renderer screeen
         world = new GameWorld();
         renderer = new GameRenderer(world,512 ,midPointY);
         
         Gdx.input.setInputProcessor(new InputHandler(world.getBall()));
     }
-
+/**
+ * renders the screen
+ * @param delta frame rate
+ */
     @Override
     public void render(float delta) {
         world.update(delta);
         renderer.render();
     }
-
+    //Most methods unused but required
+/**
+ * resizes the screen
+ * @param width
+ * @param height 
+ */
     @Override
     public void resize(int width, int height) {
-
+        //Unused 
     }
 
     @Override
