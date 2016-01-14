4d80795cce93a08e4fbbbfca46fd4d798a82e549
Der15 <daniel.ross@rogers.com>
2016-01-14 17:20:22 -0500

    Update GameRenderer.java

--- GameRenderer.java
+++ GameRenderer.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.egs.gameobjects.Ball;
 import com.egs.golfhelpers.AssetLoader;
 
+
 public class GameRenderer {
 
     private Ball ball;
@@ -20,6 +21,7 @@ public class GameRenderer {
     private ShapeRenderer shapeRenderer;
     private SpriteBatch batch;
 
+    private Texture startMenu;
     private Texture background1;
     private Texture background2;
     private Texture background3;
@@ -50,9 +52,11 @@ public class GameRenderer {
     }
 
     public void initializeAssets() {
+        startMenu = AssetLoader.ts;
         background1 = AssetLoader.lvl1;
         background2 = AssetLoader.lvl2;
-        //  background2 = AssetLoader.lvl3;
+        //  background3 = AssetLoader.lvl3;
+        //credit = AssetLoader.credits;
         golfBall = AssetLoader.ball;
     }
 
@@ -60,10 +64,14 @@ public class GameRenderer {
         // Gdx.app.log("GameRenderer", "render");
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        
         //Draw the images
         //If it is lvl1 display level1 else display the correct level
-        if (ball.getLvl() == 1) {
+        if (ball.getLvl() == 0){
+            batch.begin();
+            batch.disableBlending();
+            batch.draw(startMenu, 0, 0, 1024, 512);
+        } else if (ball.getLvl() == 1) {
             batch.begin();
             batch.disableBlending();
             batch.draw(background1, 0, 0, 1024, 512);
@@ -78,6 +86,10 @@ public class GameRenderer {
             //  batch.disableBlending();
             //  batch.draw(background3, 0, 0, 1024, 512);
             //  batch.draw(golfBall, ball.getX(), ball.getY());
+        }else if (ball.getLvl() == 20){
+         //do the high scores
+        }else if (ball.getLvl() == 21){
+         //Show the credits   
         }
         batch.enableBlending();
         batch.end();
