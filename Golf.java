34443b73875455d58434136133436acdfda00101
Der15 <daniel.ross@rogers.com>
2016-01-14 17:19:57 -0500

    Update Golf.java

--- Golf.java
+++ Golf.java
@@ -7,30 +7,18 @@ import com.egs.golfhelpers.*;
 import javax.swing.JOptionPane;
 
 public class Golf extends Game {
-    
+
     //Created game
     @Override
     public void create() {
         Gdx.app.log("Eddie's Golf Simulator", "created");
         AssetLoader.load();
+        AssetLoader.theme.play(1);
         //Display introscreen
-        
-            //Display menu option
-            int choice = Integer.parseInt(JOptionPane.showInputDialog("Hello! Would you like to: \n"
-                    + "1. Play Game \n"
-                    + "2. See Highscores \n"
-                    + "3. Credits"));
-            //Run their selection
-            if(choice == 1){
-                setScreen(new GameScreen());
-            }else if(choice == 2){
-                //Display highscore screen
-            }else if(choice == 3){
-                //Display credits
-            }
+        setScreen(new GameScreen());
     }
-    
-     @Override
+
+    @Override
     public void dispose() {
         super.dispose();
         AssetLoader.dispose();
