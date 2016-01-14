119980f8bc240a1543002daed2635415e57c7dee
Der15 <daniel.ross@rogers.com>
2016-01-13 20:05:51 -0500

    Update GameRenderer.java

--- GameRenderer.java
+++ GameRenderer.java
@@ -9,19 +9,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.egs.gameobjects.Ball;
 import com.egs.golfhelpers.AssetLoader;
 
 public class GameRenderer {
-    
+
     private Ball ball;
     private GameWorld myWorld;
     private OrthographicCamera cam;
     private ShapeRenderer shapeRenderer;
     private SpriteBatch batch;
-    
-    private Texture background;
+
+    private Texture background1;
+    private Texture background2;
+    private Texture background3;
     private Texture golfBall;
     private int midPointY;
     private int gameHeight;
@@ -44,30 +45,41 @@ public class GameRenderer {
         initializeAssets();
     }
 
-    public void initializeGameObjects(){
+    public void initializeGameObjects() {
         ball = myWorld.getBall();
     }
-    
-    public void initializeAssets(){
-        background = AssetLoader.bg;
+
+    public void initializeAssets() {
+        background1 = AssetLoader.lvl1;
+        background2 = AssetLoader.lvl2;
+        //  background2 = AssetLoader.lvl3;
         golfBall = AssetLoader.ball;
     }
-    
+
     public void render() {
-       // Gdx.app.log("GameRenderer", "render");
+        // Gdx.app.log("GameRenderer", "render");
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        
+
         //Draw the images
-        batch.begin();
-        batch.disableBlending();
-        batch.draw(background, 0, 0, 1024, 512);
-        batch.draw(golfBall, ball.getX(), ball.getY());
-        
+        //If it is lvl1 display level1 else display the correct level
+        if (ball.getLvl() == 1) {
+            batch.begin();
+            batch.disableBlending();
+            batch.draw(background1, 0, 0, 1024, 512);
+            batch.draw(golfBall, ball.getX(), ball.getY());
+        } else if (ball.getLvl() == 2) {
+            batch.begin();
+            batch.disableBlending();
+            batch.draw(background2, 0, 0, 1024, 512);
+            batch.draw(golfBall, ball.getX(), ball.getY());
+        } else if (ball.getLvl() == 3) {
+          //  batch.begin();
+            //  batch.disableBlending();
+            //  batch.draw(background3, 0, 0, 1024, 512);
+            //  batch.draw(golfBall, ball.getX(), ball.getY());
+        }
         batch.enableBlending();
         batch.end();
-        
     }
-
 }
